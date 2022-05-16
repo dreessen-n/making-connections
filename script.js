a5e69d84616696f43cde7c19a258db5b382ee90e
dreessen-n <dreessen.edu@gmail.com>
2022-05-15 21:46:36 -0600

    Write functions for accept and reject to remove

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function changeName() {
     console.log(x);
     x.innerText = 'Melisa Hidalgo';
 }
+
+// function to remove request
+
+function acceptRequest(id) {
+    console.log(id);
+    var x = document.getElementById(id);
+    console.log(x);
+    x.remove();
+}
+
+function removeRequest(id) {
+    var x = document.getElementById(id);
+    console.log(x);
+    x.remove();
+}
